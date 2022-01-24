e8427c8e944f971f7569f5cb03d8714c94e074a5
ogiwara <yushiogiwara@gmail.com>
2022-01-24 09:49:44 +0900

    update many

--- ycsb-c/graphs.bash
+++ ycsb-c/graphs.bash
@@ -1,15 +1,15 @@
 
 for name in opposite direct; do
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-throughput.png'; var=1; out_offset=7; only=0" plot.plg
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-read-throughput.png'; var=1; out_offset=21; only=0" plot.plg
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-write-throughput.png'; var=1; out_offset=24; only=0" plot.plg
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-write-throughput2.png'; var=1; out_offset=24; only=5" plot.plg
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-read-latency.png'; var=1; out_offset=11; only=0" plot.plg
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-read-latency2.png'; var=1; out_offset=11; only=3" plot.plg
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-write-latency.png'; var=1; out_offset=18; only=0" plot.plg
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-write-latency2.png'; var=1; out_offset=18; only=3" plot.plg
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-delay.png'; var=1; out_offset=14; only=0" plot.plg
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-delay2.png'; var=1; out_offset=14; only=3" plot.plg
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-stddiv.png'; var=1; out_offset=17; only=0" plot.plg
-  gnuplot -e "data='./${name}.dat'; filename='./${name}-abort.png'; var=1; out_offset=10; only=0" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-throughput.pdf'; var=1; out_offset=7; only=0" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-read-throughput.pdf'; var=1; out_offset=21; only=0" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-write-throughput.pdf'; var=1; out_offset=24; only=0" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-write-throughput2.pdf'; var=1; out_offset=24; only=5" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-read-latency.pdf'; var=1; out_offset=11; only=0" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-read-latency2.pdf'; var=1; out_offset=11; only=3" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-write-latency.pdf'; var=1; out_offset=18; only=0" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-write-latency2.pdf'; var=1; out_offset=18; only=3" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-delay.pdf'; var=1; out_offset=14; only=0" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-delay2.pdf'; var=1; out_offset=14; only=3" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-stddiv.pdf'; var=1; out_offset=17; only=0" plot.plg
+  gnuplot -e "data='./${name}.dat'; filename='./${name}-abort.pdf'; var=1; out_offset=10; only=0" plot.plg
 done
